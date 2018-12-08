07885bcb5fb1f2d142b6940bc3c5bba5ec0d63a5
Alan Yu <alanyu@alans-mbp.devices.brown.edu>
2018-12-08 16:35:08 -0500

    added some lines of code?

--- alternate-snake/TF-Agent/VAE.py
+++ alternate-snake/TF-Agent/VAE.py
@@ -26,15 +26,14 @@ class Model:
         self.images = inputs
         self.labels = outputs
 
-        self.z = encoder(inputs)
-        self.o_decoded = o_decoder(z)
-        self.kernels = decoder(z)
+        self.z = motion_encoder(inputs)
+        self.kernels = kernel_decoder(z)
 
-    def encoder(x):
+    def motion_encoder(x, difference_image):
         '''
-        Input: [batch_size , 128, 128, 3]
-
-        the 128*256 image is a concatentation of the current image and a difference image between it and the next frame
+        Input: 
+        x - current image with size [batch_size , 128, 128, 3]
+        difference_image - the difference image between the current image and the next image with size [batch_size, 128,128,3]
         
         output: [Batch Size, N_Z]
         '''
@@ -42,20 +41,33 @@ class Model:
         #add some relu or batch normalizing in between
         #figure out what they mean by a 5x5 conv
         
-        output = tf.layers.conv2d(x, filters=96,  kernel_size=(5,5), strides=(2,2), padding='SAME')
-        output = tf.layers.conv2d(output, filters=96,  kernel_size=(5,5), strides=(2,2), padding='SAME')
+
+        #concating them together to get a [batch_size, 128,128,6] image
+        output = tf.concat([x,difference_image], 3)
+
+
+
+        output = tf.layers.conv2d(x, filters=96,  kernel_size=(5,5))
+
+
+        output = tf.layers.conv2d(output, filters=96,  kernel_size=(5,5))
         output = tf.layers.batch_normalization(output)
-        output = tf.nn.leaky_relu(output) 
-        output = tf.layers.conv2d(output, filters=126,  kernel_size=(5,5), strides=(2,2), padding='SAME')
+        output = tf.layers.maxpooling(output, pool_size=[1,2,2,1], strides=[1,2,2,1])
+
+        output = tf.layers.conv2d(output, filters=126,  kernel_size=(5,5))
         output = tf.layers.batch_normalization(output)
-        output = tf.nn.leaky_relu(output) 
-        output = tf.layers.conv2d(output, filters=126,  kernel_size=(5,5), strides=(2,2), padding='SAME')
+        output = tf.layers.maxpooling(output, pool_size=[1,2,2,1], strides=[1,2,2,1])
+
+        output = tf.layers.conv2d(output, filters=126,  kernel_size=(5,5))
         output = tf.layers.batch_normalization(output)
-        output = tf.nn.leaky_relu(output) 
-        output = tf.layers.conv2d(output, filters=256,  kernel_size=(5,5), strides=(2,2), padding='SAME')
+        output = tf.layers.maxpooling(output, pool_size=[1,2,2,1], strides=[1,2,2,1])
+
+        output = tf.layers.conv2d(output, filters=256,  kernel_size=(5,5))
         output = tf.layers.batch_normalization(output)
-        output = tf.nn.leaky_relu(output) 
-        output = tf.layers.conv2d(output, filters=256,  kernel_size=(5,5), strides=(2,2), padding='SAME')
+  
+        output = tf.layers.conv2d(output, filters=256,  kernel_size=(5,5))
+        output = tf.layers.batch_normalization(output)
+        output = tf.layers.maxpooling(output, pool_size=[1,4,4,1], strides=[1,3,3,1])
 
         #what is the shape at the end of this? 256,5,5. The thing paper says it should be 256,5,5?
         reshaped = tf.reshape(output, [BATCH_SIZE, 6400]) 
@@ -72,7 +84,7 @@ class Model:
 
         return mu + tf.exp(log_sigma/2) * eps
 
-    def decoder(z):
+    def kernel_decoder(z):
         '''
         input: z of size [BATCH_SIZE, 3200]
         '''
@@ -85,13 +97,6 @@ class Model:
 
         #TODO: Figure out what this function needs to return
 
-    def o_decoder(z):
-        '''
-        input:: z of size [BATCH_SIZE, 3200]
-        '''
-
-        output = tf.reshape(z, [BATCH_SIZE, 5,5, 128])
-        #TODO - figure out if we need a normal decoder.
 
 
 
@@ -99,41 +104,38 @@ class Model:
 
 
 
+decoder_hidden = Dense(512, activation='relu')
+decoder_out = Dense(784, activation='sigmoid')
+
+h_p = decoder_hidden(z)
+outputs = decoder_out(h_p)
 
 
+# Overall VAE model, for reconstruction and training
+vae = Model(inputs, outputs)
 
-# decoder_hidden = Dense(512, activation='relu')
-# decoder_out = Dense(784, activation='sigmoid')
+# Encoder model, to encode input into latent variable
+# We use the mean as the output as it is the center point, the representative of the gaussian
+encoder = Model(inputs, mu)
 
-# h_p = decoder_hidden(z)
-# outputs = decoder_out(h_p)
+# Generator model, generate new data given latent variable z
+d_in = Input(shape=(n_z,))
+d_h = decoder_hidden(d_in)
+d_out = decoder_out(d_h)
+decoder = Model(d_in, d_out)
 
+def vae_loss(y_true, y_pred):
+    """ Calculate loss = reconstruction loss + KL loss for each data in minibatch """
+    # E[log P(X|z)]
+    recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
+    # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
+    kl = 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
 
-# # Overall VAE model, for reconstruction and training
-# vae = Model(inputs, outputs)
-
-# # Encoder model, to encode input into latent variable
-# # We use the mean as the output as it is the center point, the representative of the gaussian
-# encoder = Model(inputs, mu)
-
-# # Generator model, generate new data given latent variable z
-# d_in = Input(shape=(n_z,))
-# d_h = decoder_hidden(d_in)
-# d_out = decoder_out(d_h)
-# decoder = Model(d_in, d_out)
-
-# def vae_loss(y_true, y_pred):
-#     """ Calculate loss = reconstruction loss + KL loss for each data in minibatch """
-#     # E[log P(X|z)]
-#     recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
-#     # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
-#     kl = 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
-
-#     return recon + kl
+    return recon + kl
 
 
-# vae.compile(optimizer='adam', loss=vae_loss)
-# vae.fit(X_train, X_train, batch_size=m, nb_epoch=n_epoch)
+vae.compile(optimizer='adam', loss=vae_loss)
+vae.fit(X_train, X_train, batch_size=m, nb_epoch=n_epoch)
 
 
 
