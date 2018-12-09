a21779f313ed3d4c606523a0430d409b3db33e1e
Ben Weissmann <ben.weissmann15@gmail.com>
2018-12-09 02:23:56 -0500

    motion decoder first attempt

--- alternate-snake/TF-Agent/tmp.py
+++ alternate-snake/TF-Agent/tmp.py
@@ -4,6 +4,10 @@ class Model:
         pass 
 
 
+    def upsample_block(im_block, scale=scale):
+        pass
+        # NOTE 60k pairs of frames used for training
+
     def image_encoder(orig_image_128):
         '''
         An image encoder, which consists of convolutional layers extracting segments from the input image I
@@ -12,10 +16,10 @@ class Model:
         '''
         im_256, im_128, im_64, im_32 = generate_pyramid(im)
 
-        conv_256 = im_encode_convolutions(im_256)
-        conv_128 = im_encode_convolutions(im_128)
-        conv_64 = im_encode_convolutions(im_64)
-        conv_32 = im_encode_convolutions(im_32)
+        im_block_64 = im_encode_convolutions(im_256)
+        im_block_32 = im_encode_convolutions(im_128)
+        im_block_16 = im_encode_convolutions(im_64)
+        im_block_8 = im_encode_convolutions(im_32)
 
         # Therefore, the output size of the four channels are 32×64×64, 32×32×32, 32×16×16, and 32×8×8, respectively.  
 
@@ -38,16 +42,50 @@ class Model:
         return output
 
 
-    def cross_convolution(image_segments, motion_kernels):
+    def cross_convolution(image_block, motion_kernels):
         '''
         A cross convolutional layer takes the output of the image encoder and the kernel decoder, 
             and convolves the image segments with motion kernels.
         The cross convolutional layer has the same output size as the image encoder.
         '''
 
+        n_channels = image_block.shape[3] # 32
+        outputs = []
 
-    def motion_decoder(x):
+        for i in range(n_channels):
+            im = image_block[:,:,:,i]
+            kernel = motion_kernels[:,:,:,i]
+
+            output.append(tf.nn.conv2d(im, kernel, [1,1,1,1], "SAME"))
+
+        return tf.stack(output)
+
+
+    def motion_decoder(im_block_64, im_block_32, im_block_16, im_block_8):
+        '''
+        A motion decoder, which regresses the difference image from the combined feature maps.
         '''
 
-        '''
+        # Our motion decoder (Fig. 3e) starts with an up-sampling layer at each scale, 
+        #   making the output of all scales of the cross convolutional layer have a resolution of 64×64. 
+        upsampled_64 = upsample_block(im_block_64, scale=2)
+        upsampled_32 = upsample_block(im_block_32, scale=4)
+        upsampled_16 = upsample_block(im_block_16, scale=8)
+        upsampled_8 = upsample_block(im_block_8, scale=16)
+
+        # THIS IS INCORRECT ^^ THESE ARE NOW 128x128 not 64x64
+
+        combined_blocks = tf.concat([upsampled_64, upsampled_32, upsampled_16, upsampled_8], 3)
+
+        # This is then followed by one 9×9 and two 1×1 convolutional and batch normalization layers,
+        #     with {128, 128, 3} channels.
+        output = tf.layers.batch_normalization(tf.layers.conv2d(combined_blocks, filters=128, kernel_size=9, activation=tf.nn.relu))
+        output = tf.layers.batch_normalization(tf.layers.conv2d(output, filters=128, kernel_size=1, activation=tf.nn.relu))
+        output = tf.layers.batch_normalization(tf.layers.conv2d(output, filters=3, kernel_size=1, activation=tf.nn.relu))
+
+        return output
+
+
+
+
 
